a381d545f982a37916f4c921e853999d4d9b5432
Rakesh Baruah <rbaruah@gmail.com>
2021-10-12 13:27:35 -0500

    initialize react interface

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import {BsCalendar} from "react-icons/bs"
+function App() {
+  return (
+    <div className="App container mx-auto mt-3 font-thin">
+      <h1 className="text-5xl"><BsCalendar/>Your Appointments</h1>
+    </div>
+  );
+}
+
+export default App;
